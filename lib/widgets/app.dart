a18f26b1bd5af199423c023e880b28bcfe9b1084
Harpreet Sangar <happy_san@protonmail.com>
2021-07-21 13:14:43 +0530

    Initialize the project. * Remove the onboarding code. * Add structure to the project.

---
+++ lib/widgets/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import '../constants.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: Constants.appName),
+    );
+  }
+}
