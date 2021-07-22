2dd4235bc46c65332dbf41d3369d6c4451717d7b
Harpreet Sangar <happy_san@protonmail.com>
2021-07-22 22:12:45 +0530

    Update `title` of app.

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: Constants.appName,
+      title: '${Constants.appName} - ${Constants.appAbout}',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
