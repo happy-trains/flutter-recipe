ccc20f2622a8172650b961b2508d9c4e5b7f8639
Harpreet Sangar <happy_san@protonmail.com>
2021-11-07 19:39:50 +0530

    Add abstact UseCase base class. SearchRecipes implements UseCase.

--- lib/features/search_recipe/domain/usecases/search_recipes.dart
+++ lib/features/search_recipe/domain/usecases/search_recipes.dart
@@ -1,19 +1,32 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../entities/result.dart';
 import '../entities/filter.dart';
 import '../repositories/recipes_repository.dart';
 import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
 
-class SearchRecipes {
+class SearchRecipes implements UseCase<Result, Params> {
   final RecipesRepository repository;
 
   SearchRecipes(this.repository);
 
-  Future<Either<Failure, Result>> execute({
-    required String query,
-    required int pageNumber,
-    Filter? filter,
-  }) =>
-      repository.search(query: query, pageNumber: pageNumber, filter: filter);
+  @override
+  Future<Either<Failure, Result>> call(Params params) => repository.search(
+        query: params.query,
+        pageNumber: params.pageNumber,
+        filter: params.filter,
+      );
+}
+
+class Params extends Equatable {
+  final String query;
+  final int pageNumber;
+  final Filter? filter;
+
+  Params({required this.query, required this.pageNumber, this.filter});
+
+  @override
+  List<Object?> get props => [query, pageNumber, filter];
 }
