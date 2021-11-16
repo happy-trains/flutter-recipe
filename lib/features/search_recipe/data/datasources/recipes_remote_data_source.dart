23404e38a9d9a785ebc14f2aeac8019f7ff33198
Harpreet Sangar <happy_san@protonmail.com>
2021-11-16 20:14:08 +0530

    Add RecipesRemoteDataSource implementation.

--- lib/features/search_recipe/data/datasources/recipes_remote_data_source.dart
+++ lib/features/search_recipe/data/datasources/recipes_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:typesense/typesense.dart';
+
 import '../../../../core/error/exception.dart';
 import '../../data/models/result_model.dart';
 import '../models/filter_model.dart';
@@ -20,4 +22,80 @@ abstract class RecipesRemoteDataSource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<int> getIndexSize();
+}
+
+class RecipesRemoteDataSourceImpl implements RecipesRemoteDataSource {
+  final Client typesenseClient;
+
+  RecipesRemoteDataSourceImpl(this.typesenseClient);
+
+  @override
+  Future<int> getIndexSize() async {
+    final result =
+        await typesenseClient.collection('r').documents.search({'q': '*'});
+
+    return ResultModel.fromJson(result).found;
+  }
+
+  Map<String, dynamic> generateSearchParameters({
+    required String query,
+    required List<String> queryBy,
+    required int pageNumber,
+    FilterModel? filter,
+    List<String>? facetBy,
+    int? maxFacetValues,
+    int? perPage,
+  }) {
+    final searchParameters = <String, dynamic>{};
+
+    searchParameters.addAll({
+      'q': query,
+      'query_by': queryBy.join(','),
+      'page': pageNumber,
+    });
+
+    if (filter != null) {
+      searchParameters.addAll(filter.toApiSearchParameters());
+    }
+
+    if (facetBy != null) {
+      searchParameters.addAll({'facet_by': facetBy.join(',')});
+    }
+
+    if (maxFacetValues != null) {
+      searchParameters.addAll({'max_facet_values': maxFacetValues});
+    }
+
+    if (perPage != null) {
+      searchParameters.addAll({'per_page': perPage});
+    }
+
+    return searchParameters;
+  }
+
+  @override
+  Future<ResultModel> search({
+    required String query,
+    required List<String> queryBy,
+    required int pageNumber,
+    FilterModel? filter,
+    List<String>? facetBy,
+    int? maxFacetValues,
+    int? perPage,
+  }) async {
+    final result = await typesenseClient
+        .collection('r')
+        .documents
+        .search(generateSearchParameters(
+          query: query,
+          queryBy: queryBy,
+          pageNumber: pageNumber,
+          filter: filter,
+          facetBy: facetBy,
+          maxFacetValues: maxFacetValues,
+          perPage: perPage,
+        ));
+
+    return ResultModel.fromJson(result);
+  }
 }
---
+++ test/features/search_recipe/data/datasources/recipes_remote_data_source_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe/features/search_recipe/data/models/filter_model.dart';
+import 'package:typesense/typesense.dart';
+import 'package:mockito/annotations.dart';
+import 'package:recipe/features/search_recipe/data/datasources/recipes_remote_data_source.dart';
+
+import 'recipes_remote_data_source_test.mocks.dart';
+
+@GenerateMocks([Client])
+void main() {
+  late MockClient mockClient;
+  late RecipesRemoteDataSourceImpl remoteDataSource;
+
+  setUp(() {
+    mockClient = MockClient();
+    remoteDataSource = RecipesRemoteDataSourceImpl(mockClient);
+  });
+
+  group('generateSearchParameters', () {
+    test(
+      'should return search parameters as map',
+      () async {
+        // arrange
+        final searchParameters = {
+          'q': 'Pizza',
+          'query_by': 'title',
+          'facet_by': 'ingredient_names',
+          'max_facet_values': 1,
+          'filter_by': 'ingredient_names:=[`salt`]',
+          'page': 1,
+          'per_page': 1
+        };
+        // act
+        final map = remoteDataSource.generateSearchParameters(
+          query: 'Pizza',
+          queryBy: ['title'],
+          pageNumber: 1,
+          filter: FilterModel(
+              fieldName: 'ingredient_names', filterValues: ['salt']),
+          facetBy: ['ingredient_names'],
+          maxFacetValues: 1,
+          perPage: 1,
+        );
+        // assert
+        expect(map, equals(searchParameters));
+      },
+    );
+
+    test(
+      'should require query, queryBy and pageNumber parameters',
+      () async {
+        // arrange
+        final searchParameters = {
+          'q': 'Pizza',
+          'query_by': 'title',
+          'page': 1,
+        };
+        // act
+        final map = remoteDataSource.generateSearchParameters(
+          query: 'Pizza',
+          queryBy: ['title'],
+          pageNumber: 1,
+        );
+        // assert
+        expect(map, equals(searchParameters));
+      },
+    );
+  });
+}
