87be4f17ad6062b2a9011d9cf8f51e82db492738
Harpreet Sangar <happy_san@protonmail.com>
2021-11-19 07:51:47 +0530

    Add events and state.

--- lib/features/search_recipe/presentation/bloc/search_recipe_event.dart
+++ lib/features/search_recipe/presentation/bloc/search_recipe_event.dart
@@ -2,3 +2,17 @@ part of 'search_recipe_bloc.dart';
 
 @immutable
 abstract class SearchRecipeEvent extends Equatable {}
+
+class GetRecipes extends SearchRecipeEvent {
+  final String query;
+
+  GetRecipes(this.query);
+
+  @override
+  List<Object?> get props => [query];
+}
+
+class GetIndexSize extends SearchRecipeEvent {
+  @override
+  List<Object?> get props => [];
+}
