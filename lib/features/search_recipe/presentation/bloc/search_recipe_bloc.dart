a8dc86678169ca30016e86ccc43f8393edc89225
Harpreet Sangar <happy_san@protonmail.com>
2021-11-19 07:51:47 +0530

    Add events and state.

--- lib/features/search_recipe/presentation/bloc/search_recipe_bloc.dart
+++ lib/features/search_recipe/presentation/bloc/search_recipe_bloc.dart
@@ -1,12 +1,14 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/recipe.dart';
 
 part 'search_recipe_event.dart';
 part 'search_recipe_state.dart';
 
 class SearchRecipeBloc extends Bloc<SearchRecipeEvent, SearchRecipeState> {
-  SearchRecipeBloc() : super(SearchRecipeInitial()) {
+  SearchRecipeBloc() : super(Empty()) {
     on<SearchRecipeEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/features/search_recipe/presentation/bloc/search_recipe_state.dart
+++ lib/features/search_recipe/presentation/bloc/search_recipe_state.dart
@@ -1,6 +1,41 @@
 part of 'search_recipe_bloc.dart';
 
 @immutable
-abstract class SearchRecipeState {}
+abstract class SearchRecipeState extends Equatable {
+  final List<Object?> properties;
 
-class SearchRecipeInitial extends SearchRecipeState {}
+  SearchRecipeState(this.properties);
+
+  @override
+  List<Object?> get props => properties;
+}
+
+class Empty extends SearchRecipeState {
+  Empty() : super([]);
+}
+
+class LoadingRecipes extends SearchRecipeState {
+  LoadingRecipes() : super([]);
+}
+
+class LoadedRecipes extends SearchRecipeState {
+  final List<Recipe> recipes;
+
+  LoadedRecipes(this.recipes) : super([recipes]);
+}
+
+class LoadingIndexSize extends SearchRecipeState {
+  LoadingIndexSize() : super([]);
+}
+
+class LoadedIndexSize extends SearchRecipeState {
+  final int indexSize;
+
+  LoadedIndexSize(this.indexSize) : super([indexSize]);
+}
+
+class Error extends SearchRecipeState {
+  final String error;
+
+  Error(this.error) : super([error]);
+}
