5cee062d721c5cd01db5e4f3cec17b7adc934403
Harpreet Sangar <happy_san@protonmail.com>
2021-11-27 18:14:56 +0530

    Add color scheme.

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -12,7 +12,39 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: '${Constants.appName} - ${Constants.appAbout}',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme(
+          primary: const Color(0xfff3dfa2),
+          primaryVariant: const Color(0xffe7c763),
+          secondary: const Color(0xffb71f3a),
+          secondaryVariant: const Color(0xff801025),
+          surface: Colors.white,
+          background: Colors.white,
+          error: const Color(0xffb28c17),
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
+          onSurface: Colors.black,
+          onBackground: Colors.black,
+          onError: Colors.white,
+          brightness: Brightness.light,
+        ),
+        appBarTheme: AppBarTheme(
+          color: Color(0xfff3dfa2),
+          centerTitle: true,
+        ),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontFamily: 'Averia Serif Libre',
+            color: Colors.black,
+            fontSize: 24,
+          ),
+          headline2: TextStyle(
+            color: const Color(0xff801025),
+            fontSize: 18,
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
       ),
       home: BlocProvider(
         create: (_) => sl<SearchRecipeBloc>()..add(GetIndexSize()),
